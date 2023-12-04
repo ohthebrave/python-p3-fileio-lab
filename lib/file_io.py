825b5c0eaa5c0df2ff438849d439d8cfbb58f207
George <georgejoj28@gmail.com>
2023-12-04 20:09:01 +0300

    commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    try:
+        with open(file_name, 'w') as file:
+            file.write(file_content)
+        print(f"File '{file_name}' successfully written.")
+    except FileNotFoundError:
+        print(f"Error: Directory for file '{file_name}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def append_file(file_name, append_content):
     pass
